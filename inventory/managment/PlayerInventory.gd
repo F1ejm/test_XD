87da47e97f7e1e1df32d12faeeb284609351b115
F1ejm <karolosrodka@gmail.com>
2025-01-23 16:47:32 +0100

    Revert "nwm co to jext ale są 2 array"
    This reverts commit d7b46ca2e6e68e4d17aca4da221f831863925f76.

--- inventory/managment/PlayerInventory.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-class_name PlayerInventory
-
-@export var inventory : Array[InventoryItem]
-
-func insert(item : InventoryItem):
-	pass
--- inventory/visual/inentory_ui.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-@export var PlayerInv : PlayerInventory
-
-@onready var slot : Array = $NinePatchRect/GridContainer.get_children()
-
-
