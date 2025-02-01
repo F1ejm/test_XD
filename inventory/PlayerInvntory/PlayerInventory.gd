f3cd091e1a50177db68d808de0bc9cc1ae2c9ce4
F1ejm <karolosrodka@gmail.com>
2025-02-01 01:09:13 +0100

    działa wykrywanie czy inv[] jest pełne

--- inventory/PlayerInvntory/PlayerInventory.gd
+++ inventory/PlayerInvntory/PlayerInventory.gd
@@ -4,13 +4,12 @@ class_name PlayerInventory
 
 @export var inv : Array[Item]
 
-
+@export var is_full : int
 
 func insert(item):
 	for i in range(0,9):
 		if inv[i] == null :
 			inv[i] = item
+			is_full += 1 
 			print(inv)
 			break
-		else:
-			pass
--- inventory/item/collectable/collectable.gd
+++ inventory/item/collectable/collectable.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 @onready var sprite2D : Sprite2D = $Sprite2D
 
+@export var inventory : PlayerInventory
 @export var item : Item
 
 signal add_to_inventory
@@ -16,6 +17,8 @@ func body_setup():
 
 
 func _on_entered(body):
-	if body.has_method("Player"):
+	if body.has_method("Player") and inventory.is_full < 9 :
 		queue_free()
 		emit_signal("add_to_inventory", item)
+	else:
+		print("inventory is full")
