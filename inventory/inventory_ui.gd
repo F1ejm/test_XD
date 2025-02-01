e59ff95da36934e1e9b5e8b1763ed1613d51e06e
F1ejm <karolosrodka@gmail.com>
2025-02-01 22:25:34 +0100

    coś działa ale nwm co

--- inventory/inventory_ui.gd
+++ inventory/inventory_ui.gd
@@ -1,2 +1,11 @@
 extends Control
 
+@export var inventory : PlayerInventory
+
+@onready var slot : Array = $NinePatchRect/GridContainer.get_children()
+
+func _process(delta):
+	slot = inventory.inv 
+	for i in range(0,9):
+		slot[i]
+
